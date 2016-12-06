cb38f2a78af50edd13ff5d644cd9974ba2bcee51
DESKTOP-OART1FI\simba <simbahmso@yahoo.com>
2016-12-06 09:56:23 -0800

    Initial Commit missing user, template, includes, js, and functions folders

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>My Social Network</title>
+		<link rel="stylesheet" href="styles/style.css">
+	</head>	
+<body>
+	<!--Container starts-->
+	<div class="container">
+		<!--Head wrap starts-->
+		<div id="head_wrap">
+			<!--Header starts-->
+			<div id="header">
+				<img src="images/logo.png" style="float:left"/>
+				
+			</div>	
+			<!--Header ends-->		
+		</div>
+		<!--Head wrap ends-->
+	</div>
+	<!--Container ends-->
+</body>
+</html>
